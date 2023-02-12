fed7946dc8df55066f9cd7e548fbe35ae4e6b2bd
nashkispace <nashkispace@gmail.com>
2023-02-12 13:51:31 +0700

    add Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,100 @@
-
-
 function App() {
   return (
-    <div className="">
-       <h1 className="text-3xl font-bold underline text-center">
-      Hello world!
-    </h1>
+    
+    <div className="container mx-auto">
+     <form className="mt-20">
+        <div className="py-12">
+            <h2 className="text-2xl font-bold">Register</h2>
+            <div className="mt-8 max-w-md">
+                <div className="grid gird-cols-1 gap-6">
+                  <label className="block">
+                      <span className="text-gray-700">Full Name</span>
+                      <input
+                        type="text"
+                        className="mt-1 
+                                   block w-full 
+                                   rounded-md
+                                   bg-gray-100
+                                   border-transparant
+                                   focus:border-gray-500
+                                   focus:bg-white
+                                   focus:ring-0
+                                   "
+                        placeholder=""
+                      />
+                  </label>
+                  <label className="block">
+                      <span className="text-gray-700">Email Address</span>
+                      <input
+                        type="email"
+                        className="mt-1
+                                   block
+                                   w-full
+                                   rounded-md
+                                   bg-gray-100
+                                   border-transparant
+                                   focus:border-gray-500
+                                   focus:bg-white 
+                                     focus:ring-0
+                                "
+                                placeholder="nspace@example.com"
+                      />
+                  </label>
+                  <label className="block">
+                      <span className="text-gray-700">Birthday</span>
+                      <input
+                        type="date"
+                        className="mt-1
+                                   block
+                                   w-[20rem]
+                                   rounded-md
+                                   bg-gray-100
+                                   border-transparant
+                                   focus:border-gray-500
+                                   focus:bg-white 
+                                     focus:ring-0
+                                "
+                               
+                      />
+                  </label>
+                  <label className="block">
+                      <span className="text-gray-700">Password</span>
+                      <input
+                        type="password"
+                        className="mt-1
+                                   block
+                                   w-[20rem]
+                                   rounded-md
+                                   bg-gray-100
+                                   border-transparant
+                                   focus:border-gray-500
+                                   focus:bg-white 
+                                     focus:ring-0
+                                "
+                               placeholder="Password"
+                      />
+                  </label>
+                  <label className="block">
+                      <span className="text-gray-700">Confirm Password</span>
+                      <input
+                        type="password"
+                        className="mt-1
+                                   block
+                                   w-[20rem]
+                                   rounded-md
+                                   bg-gray-100
+                                   border-transparant
+                                   focus:border-gray-500
+                                   focus:bg-white 
+                                     focus:ring-0
+                                "
+                                placeholder="Confirm Password"
+                      />
+                  </label>
+                </div>
+            </div>
+        </div>
+    </form>
     </div>
   );
 }
